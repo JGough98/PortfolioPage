b32a931bd8dabd1930332d8a257f424ea6c0bc5e
JGough98 <94226526+JGough98@users.noreply.github.com>
2025-08-07 13:14:25 +0100

    Added the email option.

--- src/Data/SocialIconLinks.tsx
+++ src/Data/SocialIconLinks.tsx
@@ -1,3 +1,5 @@
+import EmailIcon from '../Images/Email.png';
+
 export type SocialIconLink = {
     href: string;
     imgUrl: string;
@@ -15,7 +17,12 @@ const socialLinks: SocialIconLink[] = [
         href: "https://linkedin.com/in/yourprofile",
         imgUrl: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg",
         alt: "LinkedIn"
-    }
+    },
+    {
+        href: "mailto:jwgough98@gmail.com",
+        imgUrl: EmailIcon,
+        alt: "Email"
+    },
 ];
 
 export default socialLinks;
